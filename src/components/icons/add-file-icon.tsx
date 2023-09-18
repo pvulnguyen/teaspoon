26572bff51a46da255a1841513bf5fe472891c42
pvulnguyen <pvulnguyen@gmail.com>
2023-09-18 09:31:55 -0700

    add icons

---
+++ src/components/icons/add-file-icon.tsx
@@ -0,0 +1,20 @@
+export function AddFileIcon() {
+  return (
+    <svg
+      width='32'
+      height='32'
+      viewBox='0 0 24 24'
+      strokeWidth='1.5'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+      <path d='M14 3v4a1 1 0 0 0 1 1h4'></path>
+      <path d='M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z'></path>
+      <path d='M12 11l0 6'></path>
+      <path d='M9 14l6 0'></path>
+    </svg>
+  );
+}
---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { AddFileIcon } from './add-file-icon';
+export { BookIcon } from './book-icon';
+export { DiscoverIcon } from './discover-icon';
+export { HomeIcon } from './home-icon';
+export { TrashIcon } from './trash-icon';
