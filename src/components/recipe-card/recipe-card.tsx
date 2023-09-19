a50ec35140f825813a6c7ebc2e1add31570f37b0
pvulnguyen <pvulnguyen@gmail.com>
2023-09-19 12:01:08 -0700

    update: recipe card style

--- src/components/recipe-card/recipe-card.tsx
+++ src/components/recipe-card/recipe-card.tsx
@@ -5,21 +5,23 @@ import styles from './recipe-card.module.css';
 
 export function RecipeCard({ recipe }: { recipe: any }) {
   return (
-    <Link href={`/recipes/${recipe.id}`} className={styles.card}>
-      <div className={styles.cardMedia}>
-        <Image src={recipe.image.url} alt={recipe.name} fill priority />
-      </div>
-      <div className={styles.cardBody}>
-        <h2>{recipe.name}</h2>
-        <p className={styles.description}>{recipe.description}</p>
-        <ul>
-          {recipe.categories.map((category: any, index: number) => (
-            <li key={index}>
-              <Pill mt='md'>{category.name}</Pill>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Link>
+    <div className={styles.card}>
+      <Link href={`/recipes/${recipe.id}`}>
+        <div className={styles.cardMedia}>
+          <Image src={recipe.image.url} alt={recipe.name} fill priority />
+        </div>
+        <div className={styles.cardBody}>
+          <h2>{recipe.name}</h2>
+          <p className={styles.description}>{recipe.description}</p>
+          <ul className={styles.cardFooter}>
+            {recipe.categories.map((category: { name: string }, index: number) => (
+              <li key={index}>
+                <Pill mt='md'>{category.name}</Pill>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Link>
+    </div>
   );
 }
