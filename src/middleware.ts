e1c53d4ccbf0aa60c911fa2dbfa9a8dadd801988
pvulnguyen <pvulnguyen@gmail.com>
2023-09-20 15:45:50 -0700

    update: fix protected route access with proper middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,18 @@
-import { authMiddleware } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/', '/recipes(.*)'],
   afterAuth(auth, req) {
     if (auth.userId && req.nextUrl.pathname === '/') {
       return NextResponse.redirect(new URL('/cookbook', req.url));
+    }
+    if (!auth.userId && req.nextUrl.pathname === '/recipes/new') {
+      return redirectToSignIn({ returnBackUrl: req.url });
     }
   },
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)', '/recipes/new'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/(api|trpc)(.*)'],
 };
