3f2c6127b3c90882e4640bb61ed94c87d48ae4fc
pvulnguyen <pvulnguyen@gmail.com>
2023-10-04 12:14:24 -0700

    update: public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/', '/discover', '/recipes/:id'],
 });
 
 export const config = {
