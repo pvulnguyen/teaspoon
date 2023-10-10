681076a35aede54dd86724f0e74f254de1a2111d
pvulnguyen <pvulnguyen@gmail.com>
2023-10-09 21:42:52 -0700

    add: css properies for h1 element

--- src/components/ui/main-container/main-container.tsx
+++ src/components/ui/main-container/main-container.tsx
@@ -3,7 +3,7 @@ import styles from './main-container.module.css';
 export function MainContainer({ children, title }: { children: React.ReactNode; title?: string }) {
   return (
     <main className={styles.container}>
-      {title && <h1>{title}</h1>}
+      {title && <h1 className={styles.title}>{title}</h1>}
       {children}
     </main>
   );
